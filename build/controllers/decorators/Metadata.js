8716fd332748dd5139279de33812c2290799b206
ethanblumenthal <ethan.blumenthal@gmail.com>
2019-08-08 14:29:09 -0500

    created use decorator for middlewares

---
+++ build/controllers/decorators/Metadata.js
@@ -0,0 +1,4 @@
+"use strict";
+var _a, _b, _c;
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.method = (_a = void 0, _b = _a.method, _b === void 0 ? 'method' : _b), exports.path = (_c = _a.path, _c === void 0 ? 'path' : _c);
